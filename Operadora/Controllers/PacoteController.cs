e733aa345d4d413b3c92f982fceab646d8d90230
2022-byte <a24957@ubi.pt>
2022-06-28 17:17:04 +0100

    estelização

---
+++ Operadora/Controllers/PacoteController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Operadora.Data;
+using Operadora.Models;
+
+namespace Operadora.Controllers
+{
+    public class PacoteController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PacoteController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Pacote> objPacoteList = _db.Pacotes.ToList();
+
+            return View(objPacoteList);
+        }
+    }
+}
