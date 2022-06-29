47470d052b093057477fea6186181bfe76687193
2022-byte <a24957@ubi.pt>
2022-06-29 18:06:33 +0100

    Revert "Revert "EStelização barra de menu""
    This reverts commit 8b9941a3ea3adea0c29b1b629ebc65347b396a08.

---
+++ Operadora/Controllers/OperadorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Operadora.Data;
+using Operadora.Models;
+
+namespace Operadora.Controllers
+{
+    public class OperadorController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public OperadorController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Pacote> objPacoteList = _db.Pacotes.ToList();
+
+            return View(objPacoteList);
+        }
+    }
+}
